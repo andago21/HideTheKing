6b969f48d02e38f4fea09f22dafab5f9848b1ccc
klekad19 <94104449+klekad19@users.noreply.github.com>
2025-12-04 14:23:15 +0100

    Revert "Merge remote-tracking branch 'origin/Jonejda' into Klesti"
    This reverts commit dbecbb1bec5f8acc805092d480bec99a1a1ca035.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -54,15 +54,6 @@ public class PlayerInput : MonoBehaviour
                 if (hitPiece != null && selectedPiece == null && hitPiece.isWhite == boardManager.isWhiteTurn)
                 {
                     selectedPiece = hitPiece;
-                    var hidden = HideTheKing.Core.HideTheKingManager.Instance
-                        .GetHiddenState(hitPiece.isWhite)?.HiddenTarget;
-
-                    if (hidden != null && hitPiece == hidden)
-                    {
-                        selectedPiece = hitPiece;
-                        ShowPossibleMovesHTK();
-                        return; // IMPORTANT
-                    }
                     //Debug.Log(selectedPiece.type + " has a Y of: " + selectedPiece.transform.position.y);
                     ShowPossibleMoves();
                     return;
@@ -137,16 +128,13 @@ public class PlayerInput : MonoBehaviour
                 Piece capturedPawn = boardManager.boardPieces[capturedPawnPos.x, capturedPawnPos.y];
                 if (capturedPawn != null)
                 {
-                    // Move captured pawn to graveyard instead of destroying
+                    Destroy(capturedPawn.gameObject);
                     boardManager.boardPieces[capturedPawnPos.x, capturedPawnPos.y] = null;
-
                     if (captureEffectPrefab != null)
                     {
                         Vector3 capturePos = boardManager.squares[capturedPawnPos.x * 8 + capturedPawnPos.y].position;
                         Instantiate(captureEffectPrefab, capturePos, Quaternion.identity);
                     }
-
-                    boardManager.SendToSide(capturedPawn);
                 }
             }
         }
@@ -204,15 +192,13 @@ public class PlayerInput : MonoBehaviour
         // Capture
         if (targetPiece != null)
         {
-            // Move captured target to graveyard
+            Destroy(targetPiece.gameObject);
             if (captureEffectPrefab != null) Instantiate(captureEffectPrefab, targetPos, Quaternion.identity);
-            boardManager.SendToSide(targetPiece);
         }
         else
         {
             if (moveEffectPrefab != null) Instantiate(moveEffectPrefab, targetPos, Quaternion.identity);
         }
-
 
 
         // Check if pawn moved two squares (enable en passant for next turn)
@@ -292,7 +278,9 @@ public class PlayerInput : MonoBehaviour
         );
 
         moveNotation.RecordMove(notation, boardManager.isWhiteTurn);
-        
+
+
+
         // Check all game-ending conditions
         gameRules.CheckGameEndConditions(boardManager.isWhiteTurn);
     }
@@ -371,21 +359,5 @@ public class PlayerInput : MonoBehaviour
         }
 
         Debug.Log("Pawn promoted to " + randomType + "!");
-    }
-    
-    private void ShowPossibleMovesHTK()
-    {
-        ClearHighlights();
-
-        List<Vector2Int> safeMoves = selectedPiece.GetLegalMovesHTK(boardManager.boardPieces);
-        foreach (var move in safeMoves)
-        {
-            int index = move.x * 8 + move.y;
-            Vector3 pos = boardManager.squares[index].position 
-                          + new Vector3(-0.5f, highlightPrefab.transform.position.y, +0.5f);
-
-            GameObject hl = Instantiate(highlightPrefab, pos, Quaternion.identity);
-            highlights.Add(hl);
-        }
     }
 }
